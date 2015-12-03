d5e02e5f8a930b7bf6432b17c40861c2d1b264a7
PratikPatel13 <pratikpatel3395@gmail.com>
2015-12-03 11:10:26 +0530

    Submit Commit

--- src/StrWordsinRev.cpp
+++ src/StrWordsinRev.cpp
@@ -11,7 +11,39 @@ NOTES: Don't create new string.
 */
 #include <Stdio.h>
 #include <string.h>
+void revers(char *s, int start, int end);
+/*
+	To reverse the string first reverse each word of the string and then reverse the string obtained to get the reverse words
+	Ex: String = "Hello Pratik Patel"
+		Reverse words are = "olleH kitarP letaP"
+		Reversing the above string gives result = "Patel Pratik Hello"
+*/
 
 void str_words_in_rev(char *input, int len){
+	int start = 0, end, i = 0;
+	while (i <= len)
+	{
+		if (input[i] == ' ' || input[i] == '\0'){
+			end = i - 1;
+			revers(input, start, end);
+			start = i + 1;
+		}
+
+		i++;
+	}
+	revers(input, 0, len - 1);
 	
+}
+
+void revers(char *s, int start, int end)
+{
+	char temp;
+	while (start < end)
+	{
+		temp = s[start];
+		s[start] = s[end];
+		s[end] = temp;
+		start++;
+		end--;
+	}
 }
